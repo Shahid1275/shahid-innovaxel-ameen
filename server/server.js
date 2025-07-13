522d8a263da10fbcfc24a1e9c56786e4f477949b
shahid ameen <shahidameen1275@gmail.com>
2025-07-12 21:24:44 -0700

    server project setup

--- server/server.js
+++ server/server.js
@@ -15,22 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//url routes
-app.use("/api", router);
-
-// Root
-app.get("/", (req, res) => res.send("Backend is working good!"));
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error("Uncaught error:", err.stack);
-  res.status(500).json({
-    success: false,
-    message: "Internal server error",
-    error: err.message,
-  });
-});
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
